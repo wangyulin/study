767d0c8cb860cc6a1a9130a9028cb7068f586e2f
wangyulin <jieyebing@gmail.com>
2017-01-16 14:08:10 +0800

    update root pom

--- springbootdemo/src/main/java/com/wyl/demo/controller/SampleController.java
+++ springbootdemo/src/main/java/com/wyl/demo/controller/SampleController.java
@@ -4,6 +4,8 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import java.io.PrintStream;
 
@@ -14,6 +16,9 @@ public class SampleController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
+        DispatcherServlet ds;
+        AbstractAnnotationConfigDispatcherServletInitializer a;
+        //WebConfig c;
         new PrintStream ( System.out ).println ("Hello World !");
         return "Hello World wyl!";
     }
--- springbootdemo/src/main/java/com/wyl/spring/demo/soundsystem/demo1/CompactDisc.java
+++
@@ -1,8 +0,0 @@
-package com.wyl.spring.demo.soundsystem.demo1;
-
-/**
- * Created by wangyulin on 13/01/2017.
- */
-public interface CompactDisc {
-    void play();
-}
