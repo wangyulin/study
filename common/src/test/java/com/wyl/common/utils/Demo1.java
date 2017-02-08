fc4d2185577f83b432a470c4104bdd8749b8563a
wangyulin <jieyebing@gmail.com>
2017-02-08 11:34:44 +0800

    add MapReducer with Combiner demo

---
+++ common/src/test/java/com/wyl/common/utils/Demo1.java
@@ -0,0 +1,15 @@
+package com.wyl.common.utils;
+
+/**
+ * Created by wangyulin on 07/02/2017.
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+        int a = 128;
+        int b = 2;
+        int c = 4;
+        System.out.println(((byte)a|b|c));
+    }
+
+}
